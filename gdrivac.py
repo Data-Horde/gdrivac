aafc13fc2a89104c0e2f49432829e2f2a900617c
madprogramer <themadprogramer@gmail.com>
2021-07-01 01:07:14 +0300

    Update gdrivac.py
    Added argparse for single URL

--- gdrivac.py
+++ gdrivac.py
@@ -1,8 +1,22 @@
-import requests
+import requests, argparse
 
-URL = 'https://docs.google.com/document/d/12pOhaaFh998B0kyc5Sm4IhlhIp1c9t5gDNTVVPaiJgI' 
+def immunize(URL):
+	#TODO: CHECK IF GOOGLE DRIVE LINK
+	
+	#URL = "https://docs.google.com/document/d/12pOhaaFh998B0kyc5Sm4IhlhIp1c9t5gDNTVVPaiJgI"
+	r = requests.get(URL)
 
-r = requests.get(URL)
+	print("Status for URL: {}".format(URL))
+	print(r.status_code)
+	#TODO CHECK IF non-200 status
 
-print("Status for URL: {}".format(URL))
-print(r.status_code)
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="I NEED A DESCRIPTION")
+	#TODO: ADD A DESCRIPTION
+	parser.add_argument("URL",type=str,help="Google Drive URL to immunize against")
+	#TODO: ALLOW FOR FILE INPUT INSTEAD OF URL
+	#TODO: SESSION COOKIES
+	args = parser.parse_args()
+
+	URL = (args.URL or -1)
+	immunize(URL)
