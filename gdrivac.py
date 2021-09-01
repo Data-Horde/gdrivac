82e435e790212a9f133bd7ca4d7f07239354098f
madprogramer <themadprogramer@gmail.com>
2021-09-01 17:43:30 +0200

    Added section (WIP) for long-term storage of user session cookies

--- gdrivac.py
+++ gdrivac.py
@@ -14,24 +14,45 @@ def immunize(URL,cookie_payload):
 	#TODO CHECK IF non-200 status
 
 def main():
+
+	#PARSE ARGUMENTS
+
 	parser = argparse.ArgumentParser(description="I NEED A DESCRIPTION")
 	#TODO: ADD A DESCRIPTION
 	
 	parser.add_argument("url",type=str,help="Google Drive URL to immunize against")
 	#TODO: ALLOW FOR FILE INPUT INSTEAD OF URL
 	
-	parser.add_argument("-sid",type=str,help="SID session ID")
-	parser.add_argument("-ssid",type=str,help="SSID session ID")
-	parser.add_argument("-hsid",type=str,help="HSID session ID")
-	#TODO: Read SESSION COOKIES from a file
+	parser.add_argument("-sid",type=str,help="Specify cookie value for SID")
+	parser.add_argument("-ssid",type=str,help="Specify cookie value for SSID")
+	parser.add_argument("-hsid",type=str,help="Specify cookie value for HSID")
 	
 	args = parser.parse_args()
 
+	#UNPARSE
+
 	URL = (args.url or "")
+	newCookieSID = (args.sid or "")
+	newCookieSSID = (args.ssid or "")
+	newCookieHSID = (args.hsid or "")
+
+	#TODO: Check for SESSION COOKIES file cookies.json
+
+	#TODO: If not found make one
+
+	#TODO: If cookies specified in function call arguments, overwrite
+
+	#TODO: Check and ask interactively for MISSING user session cookies
+
+	#TODO: Save cookies file
+
+	#TODO: INIT COOKIE PAYLOAD
 
 	#print(args.sid, args.ssid, args.hsid)
-	cookie_payload = {'SID':(args.sid or ""), 'SSID':(args.ssid or ""), 'HSID':(args.hsid or "")}
+	cookie_payload = {'SID':newCookieSID, 'SSID':newCookieSSID, 'HSID':newCookieHSID}
 	#print (cookie_payload)
+
+	#GET IMMUNIZED
 	immunize(URL,cookie_payload)
 
 if __name__ == '__main__':
